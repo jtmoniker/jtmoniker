68a55692a1f9af4f665a9d1eefd4beea41f45d79
Erik Tammpere <erik@Eriks-MacBook-Air.local>
2018-10-08 11:50:55 -0700

    [ADD] - Added account_payment_employeee module to account-addons/ directory.

---
+++ account-addons/account_payment_employee/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Account Payment By Employee",
+
+    'summary': """
+        Allows you to record vendor bill payments done by Employees.""",
+
+    'description': """
+    """,
+
+    'author': "JT Moniker",
+    'website': "https://www.jtmoniker.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Accounting',
+    'version': '11.0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+}
